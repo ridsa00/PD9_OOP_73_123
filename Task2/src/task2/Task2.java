090448eb2264f8635896858f9c680805750e2417
Amnaaaaa <81854874+Amnaaaaa@users.noreply.github.com>
2021-04-24 15:44:45 +0500

    commit

--- Task2/src/task2/Task2.java
+++ Task2/src/task2/Task2.java
@@ -9,15 +9,25 @@ package task2;
  *
  * @author DELL
  */
+import java.util.Scanner;
 public class Task2 {
-
+      
+      String displayVertical()
+      {
+          
+      }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("task2");
-         System.out.println("task2////");
+        System.out.println("enter the string");
+        Scanner obj=new Scanner(System.in);
+        String b=obj.nextLine();
+        
+        
+        
+        
     }
     
 }
