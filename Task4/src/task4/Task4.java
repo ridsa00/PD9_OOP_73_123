a7b42a36bead2c86cd896e1ac7776552965c80cb
ridsa00 <81819648+ridsa00@users.noreply.github.com>
2021-04-24 15:48:34 -0700

    project
    first group project

--- Task4/src/task4/Task4.java
+++ Task4/src/task4/Task4.java
@@ -17,7 +17,7 @@ public class Task4 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("task4");
-        System.out.println("task4////");
+        System.out.println("task4///");
     }
     
 }
